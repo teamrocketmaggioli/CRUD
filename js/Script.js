1fcaaecc436dd3a9108b40817c11d5e8c156e2b7
francescomomoretti <47353799+francescomomoretti@users.noreply.github.com>
2019-02-06 13:38:35 +0100

    Include javascript

--- js/Script.js
+++ js/Script.js
@@ -1,13 +1,40 @@
+
+$(document).ready(function(){
+    
+p = new persona("a","b","c","d","e");
+$("#elimina").click(function(){
+    p.Esistenza();
+});
+})
+
 class persona{
     constructor(nome, cognome, datanascita, reddito, sesso){
-        var _nome = nome;
-        var _cognome = cognome;
-        var _datanascita = datanascita;
-        var _reddito = reddito;
-        var _sesso = sesso;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.datanascita = datanascita;
+        this.reddito = reddito;
+        this.sesso = sesso;
     }
     
     controlloEsistenza(campo) {
-        return null;
+        return alert(campo);
+    }
+
+    get Esistenza(){
+        return this.controlloEsistenza();
     }
 }
+/*class Polygon {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+  
+  get area() {
+    return this.calcArea()
+  }
+
+  calcArea() {
+    return this.height * this.width;
+  }
+}*/
