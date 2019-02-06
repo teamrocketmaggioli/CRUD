53c9c3622b425579cae27d17f114aea5720d4d2e
francescomomoretti <47353799+francescomomoretti@users.noreply.github.com>
2019-02-06 15:59:45 +0100

    Versione 1.0

--- js/Script.js
+++ js/Script.js
@@ -1,46 +1,58 @@
 
 /*$(document).ready(function(){
-    
-p = new persona("a","b","c","d","e");
-$("#elimina").click(function(){
-    p.Esistenza();
-});
 })*/
-$(document).ready(function(){
-    $("#invia").click(function(){
-      Aggiungi();
-    });
-    $("#maschio").click(function(){
-      AggiornaM();
-    });
-    $("#femmina").click(function(){
-      AggiornaF();
-    })
-  });
+var persone = [];
 
-class persona{
-    constructor(nome, cognome, datanascita, reddito, sesso){
+$(document).ready(function () {
+    $("#aggiungi").click(function(){
+        var sesso;
+        if( $("#male").prop( "checked" )){
+            sesso = "Maschio";
+        }
+        else{
+            sesso = "Femmina";
+        }
+        p = new Persona($("#nome").val(), $("#cognome").val(), $("#data").val(), $("#reddito").val(), sesso);
+        persone.push(p);
+        var stampa = "";
+        for(var i = 0; i < persone.length; i++){
+            stampa += ("<tr> <td>"+persone[i].nome+"</td> <td>"+persone[i].cognome+"</td> <td>"+persone[i].datanascita+"</td> <td>"+persone[i].reddito+"</td> <td>"+persone[i].sesso+"</td> <td>Elminia</td> <td>Modifica</td> </tr>");
+        }
+        $("#tabella").html(stampa);
+    });
+});
+
+class Persona {
+    constructor(nome, cognome, datanascita, reddito, sesso) {
         this.nome = nome;
         this.cognome = cognome;
         this.datanascita = datanascita;
         this.reddito = reddito;
         this.sesso = sesso;
     }
-    
-    controlloEsistenza(campo) {
-        return alert(campo);
+
+    /*controlloEsistenza(campo) {
+        if(campo == null || campo == "undefined"){
+            return false;
+        }
+        else{
+            return true;
+        }
     }
 
-    get Esistenza(){
-        return this.controlloEsistenza();
-    }
+    controlloData(campo){
+        if(controlloEsistenza(campo)){
+            if()
+        }
+    }*/
+
 }
 /*class Polygon {
   constructor(height, width) {
     this.height = height;
     this.width = width;
   }
-  
+
   get area() {
     return this.calcArea()
   }
